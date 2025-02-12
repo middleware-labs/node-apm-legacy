4a796310bfcece94aef7ac84659d6f9c50db8619
naman-jain-15 <naman.jain@middleware.io>
2025-02-12 18:44:43 +0530

    REVERT

--- lib/src/logger.d.ts
+++
@@ -1,4 +0,0 @@
-import { Config } from './config';
-declare const log: (level: string, message: string, attributes?: Record<string, any>) => void;
-export declare const loggerInitializer: (config: Config) => void;
-export { log };
